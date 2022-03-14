0ff29f29522954c60530b409552e4bc8a13e641b
rezashokri99 <rezashokri.js@gmail.com>
2022-03-14 19:40:19 +0330

    first commit navbar ui landing ui suggestions ui completed in laptop and more devices

---
+++ src/components/Landing.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from "./Landing.module.scss";
+import { BsArrowRightShort } from "react-icons/bs";
+
+
+const Landing = () => {
+    return (
+        <div className={styles.landingContainer}>
+            <div className={styles.landingbackground}>
+            </div>
+            <div className={styles.titleContainer}>
+                <h1 className={styles.title}>Meet the <span>Juicy Lucy</span></h1>
+                <p className={styles.titleMute}>
+                    Bacon ipsum dolor amet ball tip pork chop cow tenderloin andouille. Pastrami pork picanha tongue venison strip steak
+                </p>
+                <a href='#1' className={styles.orderBtn}>
+                    Order Now
+                    <BsArrowRightShort />
+                </a>
+            </div>
+        </div>
+    );
+}
+ 
+export default Landing;
---
+++ src/components/Suggestions.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from "./Suggestions.module.scss";
+import burgerImage from "../images/1.jpg";
+import friesImage from "../images/2.jpg";
+import dessertImage from "../images/3.jpg";
+
+
+const Suggestions = () => {
+    return (
+        <div className={styles.suggestionsContainer}>
+            <div className={styles.suggestionsContent}>
+                
+                <div className={styles.col_6}>
+                    <a href='#3' className={styles.burgerSuggestion}>
+                        <img src={burgerImage} alt="burger_image" />
+                        <div className={styles.titleSuggestion}>
+                            <h5>Burgers</h5>
+                            <h4>Just perfect</h4>
+                        </div>
+                    </a>
+                </div>
+
+                <div className={styles.col_6}>
+                    <a href='#3' className={styles.friesSuggestion}>
+                        <img src={friesImage} alt="burger_image" />
+                        <div className={styles.titleSuggestion}>
+                            <h5>Fries</h5>
+                            <h4>Crunchy delight</h4>    
+                        </div>
+                    </a>
+                    <a href='#3' className={styles.dessertSuggestion}>
+                        <img src={dessertImage} alt="burger_image" />
+                        <div className={styles.titleSuggestion}>
+                            <h5>Desserts</h5>
+                            <h4>Mouth watering</h4>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+ 
+export default Suggestions;
